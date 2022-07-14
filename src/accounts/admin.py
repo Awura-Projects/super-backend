32c5f413a8a73e669e88aba15a85ec6e4af20147
Mohammed Ali <mohaali482@gmail.com>
2022-07-14 13:31:56 +0300

    Add models to admin site

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Employee, Supplier, Delivery
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Delivery)
+class DeliveryAdmin(admin.ModelAdmin):
+    pass
