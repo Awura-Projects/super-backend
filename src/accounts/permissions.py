009b22590a2b77f7111f70635c0087f62d4c6bdc
Mohammed Ali <mohaali482@gmail.com>
2022-07-14 15:17:37 +0300

    Add permissions file

---
+++ src/accounts/permissions.py
@@ -0,0 +1,49 @@
+from rest_framework import permissions
+
+class IsAdminOrSelfEmployee(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        user = request
+        if not user.is_authenticated:
+            return False
+
+        if user.is_staff:
+            return True
+
+        employee = user.employee
+
+        if employee == obj:
+            return True
+
+        return False
+
+class IsAdminOrSelfSupplier(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        user = request
+        if not user.is_authenticated:
+            return False
+
+        if user.is_staff:
+            return True
+
+        supplier = user.supplier
+
+        if supplier == obj:
+            return True
+
+        return False
+
+class IsAdminOrSelfDelivery(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        user = request
+        if not user.is_authenticated:
+            return False
+
+        if user.is_staff:
+            return True
+
+        delivery = user.delivery
+
+        if delivery == obj:
+            return True
+
+        return False
