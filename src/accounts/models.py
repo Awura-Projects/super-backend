a9d3056f8ca1a5ab62ffde38874277221c8bf578
Mohammed Ali <mohaali482@gmail.com>
2022-07-14 13:11:53 +0300

    Add customer model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+from timestamps.models import Model
 
-# Create your models here.
+class Customer(Model):
+    """
+    Customer model to store customer's information
+    """
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_("User"),
+        on_delete=models.CASCADE
+    )
+    address = models.CharField(_("City"), max_length=250)
