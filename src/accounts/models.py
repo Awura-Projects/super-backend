57a67e74e821c060bedf7c2d433398abce4cf323
Mohammed Ali <mohaali482@gmail.com>
2022-07-14 13:29:57 +0300

    Add delivery model and Remove customer model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,16 +3,6 @@ from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from timestamps.models import Model
 
-class Customer(Model):
-    """
-    Customer model to store customer's information
-    """
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        verbose_name=_("User"),
-        on_delete=models.CASCADE
-    )
-
 class Employee(Model):
     """
     Employee model to store employee information
@@ -22,7 +12,9 @@ class Employee(Model):
         verbose_name=_("User"),
         on_delete=models.CASCADE
     )
+    profile_picture = models.ImageField(_("Profile Picture"), upload_to='accounts/employee/profile_pictures/')
     birthdate = models.DateField(_("Birthdate"), auto_now=False, auto_now_add=False)
+    identification_card = models.ImageField(_("Id Card"), upload_to='accounts/employee/id/')
 
 class Supplier(Model):
     """
@@ -33,3 +25,19 @@ class Supplier(Model):
         verbose_name=_("User"),
         on_delete=models.CASCADE
     )
+    profile_picture = models.ImageField(_("Profile Picture"), upload_to='accounts/supplier/profile_pictures/')
+    birthdate = models.DateField(_("Birthdate"), auto_now=False, auto_now_add=False)
+    identification_card = models.ImageField(_("Id Card"), upload_to='accounts/supplier/id/')
+
+class Delivery(Model):
+    """
+    Delivery model to store supplier basic information
+    """
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_("User"),
+        on_delete=models.CASCADE
+    )
+    profile_picture = models.ImageField(_("Profile Picture"), upload_to='accounts/delivery/profile_pictures/')
+    birthdate = models.DateField(_("Birthdate"), auto_now=False, auto_now_add=False)
+    identification_card = models.ImageField(_("Id Card"), upload_to='accounts/delivery/id/')
