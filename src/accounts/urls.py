837c999785f40163a19af4d57ef050cf7b8470a0
Mohammed Ali <mohaali482@gmail.com>
2022-07-15 21:40:35 +0300

    Add password change url

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 
-from .views import CustomerSignupAPIView, CustomerUpdateAPIView
+from .views import (
+    CustomerSignupAPIView, CustomerUpdateAPIView,
+    PasswordChangeAPIView
+)
 
 urlpatterns = [
     path('', include("accounts.routers")),
+    path('change-password/', PasswordChangeAPIView.as_view(), name='change-password'),
     path('customer/signup/', CustomerSignupAPIView.as_view(), name='customer-signup'),
     path('customer/update/', CustomerUpdateAPIView.as_view(), name='customer-update'),
 ]
