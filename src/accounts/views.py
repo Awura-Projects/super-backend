c463327c8aea11f46ff6a6c873cb14cebc1bf14f
Mohammed Ali <mohaali482@gmail.com>
2022-07-15 21:17:10 +0300

    Add signup and update api views for customers

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import views
+from rest_framework.response import Response
 
-# Create your views here.
+from authentication.serializers import UserSerializer
+from .permissions import IsCustomer
+from .serializers import CustomerSignupForm, CustomerUpdateForm
+
+class CustomerSignupAPIView(views.APIView):
+    serializer_class = CustomerSignupForm
+
+    def post(self, request, format=None):
+        serializer = CustomerSignupForm(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.save()
+        user_serializer = UserSerializer(instance=user)
+
+        return Response(user_serializer.data)
+
+
+class CustomerUpdateAPIView(views.APIView):
+    serializer_class = CustomerUpdateForm
+    permission_classes = [IsCustomer]
+
+    def get(self, request):
+        user = request.user
+        serializer = CustomerUpdateForm(instance=user)
+
+        return Response(serializer.data)
+
+    def put(self, request, pk=None):
+        user = request.user
+        serializer = CustomerUpdateForm(instance=user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data)
