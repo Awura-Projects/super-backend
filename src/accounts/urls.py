2a9eb7048009dc99ee796809c61611dc802be6ec
Mohammed Ali <mohaali482@gmail.com>
2022-07-15 21:17:42 +0300

    Add urls for signup and update api view for customers

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
 
+from .views import CustomerSignupAPIView, CustomerUpdateAPIView
+
 urlpatterns = [
     path('', include("accounts.routers")),
+    path('customer/signup/', CustomerSignupAPIView.as_view(), name='customer-signup'),
+    path('customer/update/', CustomerUpdateAPIView.as_view(), name='customer-update'),
 ]
