abf86a11615852f0bc06cad175cd4f3d5e4c81fb
Mohammed Ali <mohaali482@gmail.com>
2022-07-16 19:35:37 +0300

    Add customer retrieve view and url

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 from .views import (
     CustomerSignupAPIView, CustomerUpdateAPIView,
-    PasswordChangeAPIView
+    PasswordChangeAPIView, CustomerRetrieveAPIView
 )
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('change-password/', PasswordChangeAPIView.as_view(), name='change-password'),
     path('customer/signup/', CustomerSignupAPIView.as_view(), name='customer-signup'),
     path('customer/update/', CustomerUpdateAPIView.as_view(), name='customer-update'),
+    path('customer/<int:pk>/', CustomerRetrieveAPIView.as_view(), name='customer-detail'),
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,9 +1,15 @@
-from rest_framework import views, permissions
+from django.contrib.auth import get_user_model
+from rest_framework import views, generics, permissions
 from rest_framework.response import Response
 
 from authentication.serializers import UserSerializer, PasswordChangeForm
-from .permissions import IsCustomer
+from .permissions import (
+    IsSelfCustomer,
+    IsAdminOrDeliveryOrSelfCustomer,
+)
 from .serializers import CustomerSignupForm, CustomerUpdateForm
+
+User = get_user_model()
 
 class CustomerSignupAPIView(views.APIView):
     serializer_class = CustomerSignupForm
@@ -20,7 +26,7 @@ class CustomerSignupAPIView(views.APIView):
 
 class CustomerUpdateAPIView(views.APIView):
     serializer_class = CustomerUpdateForm
-    permission_classes = [IsCustomer]
+    permission_classes = [IsSelfCustomer]
 
     def get(self, request):
         user = request.user
@@ -49,3 +55,11 @@ class PasswordChangeAPIView(views.APIView):
         serializer.save()
 
         return Response(serializer.data)
+
+class CustomerRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [
+        permissions.IsAuthenticated,
+        IsAdminOrDeliveryOrSelfCustomer,
+    ]
