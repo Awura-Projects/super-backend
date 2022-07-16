635e541ac12e8e0b509682da592db840f3169310
Mohammed Ali <mohaali482@gmail.com>
2022-07-16 19:47:07 +0300

    Add cart and cartitem serializer

---
+++ src/orders/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from .models import Cart, CartItem
+
+class CartItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CartItem
+        fields = (
+            'product',
+            'quantity',
+            'unit_price',
+            'discount',
+        )
+
+class CartSerializer(serializers.ModelSerializer):
+    user = serializers.HyperlinkedIdentityField("customer-detail")
+    delivery_man = serializers.HyperlinkedIdentityField("delivery-detail")
+    items = CartItemSerializer(many=True)
+    class Meta:
+        model = Cart
+        fields = (
+            'user',
+            'order_date',
+            'delivery_man',
+            'status',
+            'items',
+        )
