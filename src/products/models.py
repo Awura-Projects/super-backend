ec0992a1308a54b4f355adcac15051c2a3e1c37b
Mohammed Ali <mohaali482@gmail.com>
2022-07-16 17:54:00 +0300

    Update model type to include timestamps

--- src/products/models.py
+++ src/products/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from timestamps.models import Model
 
 # Create your models here.
-class Producer(models.Model):
+class Producer(Model):
     title = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title
 
-class Product(models.Model):
+class Product(Model):
     productName = models.CharField(max_length=100)
     productType = models.CharField(max_length=100)
     Description = models.TextField(max_length=100)
