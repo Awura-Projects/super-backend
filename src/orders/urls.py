2d7bb8e87b404bb7e7a15676db968cedf6be6075
Mohammed Ali <mohaali482@gmail.com>
2022-07-17 21:08:24 +0300

    Add cart close api view

--- src/orders/urls.py
+++ src/orders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from .routers import router
-from .views import CartRetrieveAPIView
+from .views import CartRetrieveAPIView, CartCloseAPIView
 
 urlpatterns = [
     path('cart/<int:pk>/', CartRetrieveAPIView.as_view(), name='cart-detail'),
+    path('cart/', CartCloseAPIView.as_view(), name='cart-close'),
 ]
 
 urlpatterns += router.urls
--- src/orders/views.py
+++ src/orders/views.py
@@ -1,4 +1,7 @@
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from rest_framework import generics, views, permissions
+from rest_framework.response import Response
 
 from .models import Cart
 from .permissions import IsSelfCart
@@ -6,10 +9,30 @@ from .serializers import CartSerializer
 
 class CartRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = CartSerializer
-    permission_classes = [IsSelfCart]
+    permission_classes = [
+        permissions.IsAuthenticated,
+        IsSelfCart
+    ]
 
     def get_queryset(self):
         user = self.request.user
 
         queryset = Cart.objects.filter(user=user)
         return queryset
+
+class CartCloseAPIView(views.APIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+        IsSelfCart
+    ]
+
+    def post(self, request, format=None):
+        cart_id = request.data.get('cart')
+        cart = get_object_or_404(Cart, pk=cart_id)
+
+        cart.closed = True
+        cart.closed_time = timezone.now()
+        cart.save()
+        serializer = CartSerializer(cart)
+
+        return Response(serializer)
