775e1a4da480894043765e1d5dd0bf2301ccdd26
Mohammed Ali <mohaali482@gmail.com>
2022-07-17 19:28:18 +0300

    Update the admin fields of models of products

--- src/products/admin.py
+++ src/products/admin.py
@@ -2,5 +2,29 @@ from django.contrib import admin
 
 from .models import Product, Producer
 
-admin.site.register(Product)
-admin.site.register(Producer)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            "fields": (
+                'producer',
+                'posted_by',
+                'productName',
+                'productType',
+                'description',
+                'amount',
+                'price',
+                'discount',
+            ),
+        }),
+    )
+
+@admin.register(Producer)
+class ProducerAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            "fields": (
+                'title',
+            ),
+        }),
+    )
