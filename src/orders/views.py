62c181ecc71a5af8266a132a4c35a554baa873e3
Mohammed Ali <mohaali482@gmail.com>
2022-07-17 19:30:29 +0300

    Add Cart retrieve api view

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Cart
+from .permissions import IsSelfCart
+from .serializers import CartSerializer
+
+class CartRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = CartSerializer
+    permission_classes = [IsSelfCart]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        queryset = Cart.objects.filter(user=user)
+        return queryset
