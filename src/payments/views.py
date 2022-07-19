e3915215eb14e8777a9ed3b68d2fcdeed3477256
Mohammed Ali <mohaali482@gmail.com>
2022-07-19 12:56:16 +0300

    Add payment retrieve api view

--- src/payments/views.py
+++ src/payments/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, permissions
 
+from .models import Payment
 from .serializers import PaymentSerailzer
 
 class PaymentCreateAPIView(generics.CreateAPIView):
@@ -7,3 +8,15 @@ class PaymentCreateAPIView(generics.CreateAPIView):
     permission_classes = [
         permissions.IsAuthenticated,
     ]
+
+class PaymentRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = PaymentSerailzer
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
+    def get_queryset(self):
+        user = self.request
+        queryset = Payment.objects.filter(user=user)
+
+        return queryset
