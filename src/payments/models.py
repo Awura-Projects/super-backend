a26071d16258e57a467c768439d7984a85594db6
Mohammed Ali <mohaali482@gmail.com>
2022-07-19 11:29:56 +0300

    Add payment model

--- src/payments/models.py
+++ src/payments/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from timestamps.models import Model
 
-# Create your models here.
+class Payment(Model):
+    customer = models.ForeignKey("authentication.User", verbose_name=_("Customer"), on_delete=models.CASCADE)
+    cart = models.OneToOneField("orders.Cart", verbose_name=_("Cart"), on_delete=models.CASCADE)
+    total_amount = models.DecimalField(_("Total Amount"), max_digits=10, decimal_places=2)
+    payment_date = models.DateTimeField(_("Payment Date"), auto_now=False, auto_now_add=False)
+    reciept = models.ImageField(_("Receipt"), upload_to='payments/reciept', blank=True, null=True)
