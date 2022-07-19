4f8bb3787659c4fc7296adbcabda1fc372f02c8f
Mohammed Ali <mohaali482@gmail.com>
2022-07-19 12:55:40 +0300

    Update customer field to user field

--- src/payments/models.py
+++ src/payments/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from timestamps.models import Model
 
 class Payment(Model):
-    customer = models.ForeignKey("authentication.User", verbose_name=_("Customer"), on_delete=models.CASCADE)
+    user = models.ForeignKey("authentication.User", verbose_name=_("User"), on_delete=models.CASCADE)
     cart = models.OneToOneField("orders.Cart", verbose_name=_("Cart"), on_delete=models.CASCADE)
     total_amount = models.DecimalField(_("Total Amount"), max_digits=10, decimal_places=2)
     payment_date = models.DateTimeField(_("Payment Date"), auto_now=False, auto_now_add=False)
