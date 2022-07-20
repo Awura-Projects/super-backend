4fb0908373dd966f295e75cca803f20e3201880c
Mohammed Ali <mohaali482@gmail.com>
2022-07-20 13:49:35 +0300

    Remove reciept field and Add card number and card holder fields

--- src/payments/models.py
+++ src/payments/models.py
@@ -5,6 +5,7 @@ from timestamps.models import Model
 class Payment(Model):
     user = models.ForeignKey("authentication.User", verbose_name=_("User"), on_delete=models.CASCADE)
     cart = models.OneToOneField("orders.Cart", verbose_name=_("Cart"), on_delete=models.CASCADE)
+    card_number = models.CharField(_("Credit Card Number"), max_length=50)
+    card_holder = models.CharField(_("Card Holder"), max_length=50)
     total_amount = models.DecimalField(_("Total Amount"), max_digits=10, decimal_places=2)
     payment_date = models.DateTimeField(_("Payment Date"), auto_now=False, auto_now_add=False)
-    reciept = models.ImageField(_("Receipt"), upload_to='payments/reciept')
