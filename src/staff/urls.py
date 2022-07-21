b88f847fd8164c3d0e00757c8532dddc7145fb05
Mohammed Ali <mohaali482@gmail.com>
2022-07-21 20:05:06 +0300

    Add order router to staff urls

--- src/staff/urls.py
+++ src/staff/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from orders.admin_routers import router as order_router
 from payments.routers import router as payment_router
 
 urlpatterns = []
 
 urlpatterns += payment_router.urls
+urlpatterns += order_router.urls
