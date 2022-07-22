761aedf46b60197e51bfb9c8fadf1e9c7772ad9f
Mohammed Ali <mohaali482@gmail.com>
2022-07-22 16:07:32 +0300

    Add accounts router to staff urls

--- src/staff/urls.py
+++ src/staff/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from accounts.admin_routers import router as account_router
 from orders.admin_routers import router as order_router
 from payments.admin_routers import router as payment_router
 
@@ -7,3 +8,4 @@ urlpatterns = []
 
 urlpatterns += payment_router.urls
 urlpatterns += order_router.urls
+urlpatterns += account_router.urls
