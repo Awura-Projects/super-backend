6ea51a5c02933b9852bd8bcb4e40cf39be8ec525
Mohammed Ali <mohaali482@gmail.com>
2022-07-22 16:08:37 +0300

    Update routers to plural app name

--- src/staff/urls.py
+++ src/staff/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from accounts.admin_routers import router as account_router
-from orders.admin_routers import router as order_router
-from payments.admin_routers import router as payment_router
+from accounts.admin_routers import router as accounts_router
+from orders.admin_routers import router as orders_router
+from payments.admin_routers import router as payments_router
 
 urlpatterns = []
 
-urlpatterns += payment_router.urls
-urlpatterns += order_router.urls
-urlpatterns += account_router.urls
+urlpatterns += accounts_router.urls
+urlpatterns += orders_router.urls
+urlpatterns += payments_router.urls
