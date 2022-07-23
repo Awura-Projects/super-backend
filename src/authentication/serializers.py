378f9e69d414713371cf8c96b106e45252057186
Mohammed Ali <mohaali482@gmail.com>
2022-07-23 19:03:06 +0300

    Add change password form

--- src/authentication/serializers.py
+++ src/authentication/serializers.py
@@ -105,3 +105,28 @@ class PasswordChangeForm(serializers.Serializer):
 
 class PasswordResetForm(serializers.Serializer):
     email = serializers.EmailField()
+
+
+class ChangePasswordForm(serializers.Serializer):
+    new_password = serializers.CharField(
+        write_only=True, style={'input_type': 'password'})
+    confirm_password = serializers.CharField(
+        write_only=True, style={'input_type': 'password'})
+
+    def validate_new_password(self, value):
+        validate_password(value)
+
+        return value
+
+    def validate(self, attrs):
+        new_password = attrs.get('new_password')
+        confirm_password = attrs.get('confirm_password')
+
+        if new_password != confirm_password:
+            raise serializers.ValidationError(
+                {
+                    'new_password': 'Passwords don\'t match',
+                }
+            )
+
+        return attrs
