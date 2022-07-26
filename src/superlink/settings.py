0194f7f3834ecd073583ffd6c505597c0e6566bb
Mohammed Ali <mohaali482@gmail.com>
2022-07-26 13:20:38 +0300

    Add session authentication for testing purpose

--- src/superlink/settings.py
+++ src/superlink/settings.py
@@ -179,6 +179,7 @@ SIMPLE_JWT = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
