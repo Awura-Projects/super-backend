5150c5155a8538dd90bef2aeb6af26bcfd035da8
Mohammed Ali <mohaali482@gmail.com>
2022-08-12 11:27:02 +0300

    Add a collectstatic command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 /opt/venv/bin/python manage.py migrate --no-input
+/opt/venv/bin/python manage.py collectstatic --no-input
 
 # /opt/venv/bin/gunicorn superlink.wsgi:application --bind 0.0.0.0:8000
 /opt/venv/bin/python manage.py runserver 0.0.0.0:8000
