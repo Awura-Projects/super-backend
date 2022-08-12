a3d5eeaf7ec82b3e5fdada1f15a7900c153ca073
Mohammed Ali <mohaali482@gmail.com>
2022-08-12 11:17:16 +0300

    Update the python to use virtual env python

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-python manage.py migrate --no-input
+/opt/venv/bin/python manage.py migrate --no-input
 
-opt/venv/bin/gunicorn superlink.wsgi:application --bind 0.0.0.0:8000
+# /opt/venv/bin/gunicorn superlink.wsgi:application --bind 0.0.0.0:8000
+/opt/venv/bin/python manage.py runserver 0.0.0.0:8000
