1d23217d47a3a8c205e2eebc1d25559b5d640adc
Mohammed Ali <mohaali482@gmail.com>
2022-08-13 23:36:02 +0300

    Update the server from development server to gunicorn server

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 /opt/venv/bin/python manage.py migrate --no-input
 /opt/venv/bin/python manage.py collectstatic --no-input
 
-# /opt/venv/bin/gunicorn superlink.wsgi:application --bind 0.0.0.0:8000
-/opt/venv/bin/python manage.py runserver 0.0.0.0:8000
+/opt/venv/bin/gunicorn superlink.wsgi:application --bind 0.0.0.0:8000
+# /opt/venv/bin/python manage.py runserver 0.0.0.0:8000
