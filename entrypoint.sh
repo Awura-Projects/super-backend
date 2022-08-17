6fc4fea859dbf369711802517c3489b34af317bc
Mohammed Ali <mohaali482@gmail.com>
2022-08-17 17:40:04 +0300

    Remove unnecessary comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,3 @@
 /opt/venv/bin/python manage.py collectstatic --no-input
 
 /opt/venv/bin/gunicorn superlink.wsgi:application --bind 0.0.0.0:8000
-# /opt/venv/bin/python manage.py runserver 0.0.0.0:8000
