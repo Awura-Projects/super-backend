7bf54544e2f3a15919e382fc8df9872ef8475f7f
Mohammed Ali <mohaali482@gmail.com>
2022-09-16 21:33:53 +0300

    Add a setting to rotate and blacklist a refresh token

--- src/superlink/settings.py
+++ src/superlink/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'crispy_forms',
     'corsheaders',
+    'rest_framework_simplejwt.token_blacklist',
 
     # local application
     'authentication',
@@ -194,6 +195,8 @@ VALID_CARD_NUMBERS = [
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True
 }
 
 REST_FRAMEWORK = {
